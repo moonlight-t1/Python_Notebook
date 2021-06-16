d7ecdae080632731e544f9773fb3c5671e627681
Tae-won Seo <t1won.seo@gmail.com>
2021-06-16 11:31:54 +0900

    코딩도장 복습

---
+++ 01_Python_Basic/CodingDojang/practice/05_숫자 계산하기.py
@@ -0,0 +1,15 @@
+# 1. divmod
+a = 5
+b = 2
+
+print(divmod(5, 2))  # return as a tuple
+
+# 2. complex number
+print(1.2 + 1.3j)  # print complex number
+
+print(complex(1.2, 1.3))  # make complex number
+
+# ex
+print(10 / 4)
+print(float(10 // 3))
+print(7 + (10 - 5) * 2)
--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Python Notebook
 
+- [Python Docs](https://docs.python.org/3/)
+
 ## References
+- [레벨업 파이썬](https://wikidocs.net/book/4170)
 - 처음 시작하는 파이썬
 - Effective Python
